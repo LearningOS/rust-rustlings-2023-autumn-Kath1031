98da2ddbfa1b846bf93abca2c22e00264a5451c0
kath1031 <1203306166@qq.com>
2023-10-09 17:34:51 +0800

    first commit

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples (apple_cnt:i32) -> i32 {
+    if apple_cnt <= 40 {
+        apple_cnt * 2
+    } else {
+        apple_cnt
+    }
+
+}
 
 // Don't modify this function!
 #[test]
