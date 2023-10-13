07c5b94d439a991d2418071515e4dfcadcaaf361
kath1031 <1203306166@qq.com>
2023-10-13 17:56:29 +0800

    2023/10/13

---
+++ exercises/clippy/target/debug/deps/clippy1-3effa72a6166e214.d
@@ -0,0 +1,9 @@
+/home/ubuntu/桌面/123/rust-rustlings-2023-autumn-Kath1031/exercises/clippy/target/debug/deps/clippy1-3effa72a6166e214.rmeta: clippy1.rs Cargo.toml
+
+/home/ubuntu/桌面/123/rust-rustlings-2023-autumn-Kath1031/exercises/clippy/target/debug/deps/clippy1-3effa72a6166e214.d: clippy1.rs Cargo.toml
+
+clippy1.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
