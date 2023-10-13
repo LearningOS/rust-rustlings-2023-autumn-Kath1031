6c3786935a9b7c090a32d6ff66f972b537d31ee8
kath1031 <1203306166@qq.com>
2023-10-13 15:39:21 +0800

    2023/10/13.15.39

---
+++ target/debug/deps/log-737944cd688fcf0b.d
@@ -0,0 +1,9 @@
+/home/ubuntu/桌面/123/rust-rustlings-2023-autumn-Kath1031/target/debug/deps/log-737944cd688fcf0b.rmeta: /home/ubuntu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.19/src/lib.rs /home/ubuntu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.19/src/macros.rs /home/ubuntu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.19/src/serde.rs
+
+/home/ubuntu/桌面/123/rust-rustlings-2023-autumn-Kath1031/target/debug/deps/liblog-737944cd688fcf0b.rlib: /home/ubuntu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.19/src/lib.rs /home/ubuntu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.19/src/macros.rs /home/ubuntu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.19/src/serde.rs
+
+/home/ubuntu/桌面/123/rust-rustlings-2023-autumn-Kath1031/target/debug/deps/log-737944cd688fcf0b.d: /home/ubuntu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.19/src/lib.rs /home/ubuntu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.19/src/macros.rs /home/ubuntu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.19/src/serde.rs
+
+/home/ubuntu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.19/src/lib.rs:
+/home/ubuntu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.19/src/macros.rs:
+/home/ubuntu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.19/src/serde.rs:
