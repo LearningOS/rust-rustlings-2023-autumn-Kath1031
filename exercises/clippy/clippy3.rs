862046278bb7dd3efb194989e940f48e3c54ac72
kath1031 <1203306166@qq.com>
2023-10-13 19:08:53 +0800

    2023/10/13

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -9,12 +9,13 @@
 #[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
-    if my_option.is_none() {
-        my_option.unwrap();
-    }
+    // if my_option.is_none() {
+    //     my_option.unwrap();
+    // }
+
 
     let my_arr = &[
-        -1, -2, -3
+        -1, -2, -3,
         -4, -5, -6
     ];
     println!("My array! Here it is: {:?}", my_arr);
@@ -25,7 +26,6 @@ fn main() {
     let mut value_a = 45;
     let mut value_b = 66;
     // Let's swap these two!
-    value_a = value_b;
-    value_b = value_a;
+    std::mem::swap(&mut value_a,&mut value_b);
     println!("value a: {}; value b: {}", value_a, value_b);
 }
---
+++ exercises/clippy/target/debug/deps/clippy3-0d24fc872c685e6e.d
@@ -0,0 +1,9 @@
+/home/ubuntu/桌面/123/rust-rustlings-2023-autumn-Kath1031/exercises/clippy/target/debug/deps/clippy3-0d24fc872c685e6e.rmeta: clippy3.rs Cargo.toml
+
+/home/ubuntu/桌面/123/rust-rustlings-2023-autumn-Kath1031/exercises/clippy/target/debug/deps/clippy3-0d24fc872c685e6e.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
